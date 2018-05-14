cdddff58c79d3abf3e852851f20b44c319f119ed
amsedivy <amsedivy@gmail.com>
2018-05-14 14:16:52 -0400

    add metronome
    tighten up vars by using this (so eslint is less whiny)

--- src/client/audio/conductor.js
+++ src/client/audio/conductor.js
@@ -1,26 +1,27 @@
 /** this will be the manager of the playback streams */
 import Indeterminancy from '../../control/indeterminanceManager';
 import Stream from './playbackStream';
+import Metronome from './metronome';
 import EventDispatcher from '../../control/centralDispatch';
 import EvtTypes from '../../model/enum/eventTypes';
 
 const chanceMngr = new Indeterminancy();
-const streams = [];
 let instance = null;
-let id;
 
 class Conudctor {
   constructor() {
     if (!instance) {
       instance = this;
       const { numStreams } = chanceMngr;
+      this.streamList = [];
+      this.metronome = new Metronome();
 
       for (let i = 0; i < numStreams; i += 1) {
         const stream = new Stream();
-        streams.push(stream);
+        this.streamList.push(stream);
       }
 
-      id = Math.floor(Math.random() * 999);
+      this.uid = Math.floor(Math.random() * 999);
 
       EventDispatcher.addListener(EvtTypes.SECTION_COMPLETE, this.handleSectionComplete);
     }
@@ -28,16 +29,20 @@ class Conudctor {
     return this;
   }
 
+  initPeformance() {
+    this.metronome.start();
+  }
+
   handleSectionComplete(evt) {
     console.log(evt);
   }
 
   get streams() {
-    return [].concat(streams);
+    return [].concat(this.streamList);
   }
 
   get id() {
-    return id;
+    return this.uid;
   }
 }
 

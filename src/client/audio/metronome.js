8fb9a7fe5dc6aac739f497edc228cacf80910bc6
amsedivy <amsedivy@gmail.com>
2018-05-14 14:15:01 -0400

    tighten up

---
+++ src/client/audio/metronome.js
@@ -0,0 +1,26 @@
+import EventDispatcher from '../../control/centralDispatch';
+import EventTypes from '../../model/enum/eventTypes';
+
+class Metronome {
+  constructor() {
+    // set the timespan of a quarternote to a value between 450-550 milliseconds
+    this.quaternote = Math.floor(Math.random() * 100) + 450;
+  }
+
+  start() {
+    // begin the interval
+    this.counter = setInterval(() => {
+      EventDispatcher.disptchEvent(EventTypes.BEAT);
+    }, this.quaternote);
+
+    // fire the first beat
+    EventDispatcher.disptchEvent(EventTypes.BEAT);
+  }
+
+  stop() {
+    // stop the interval
+    clearInterval(this.counter);
+  }
+}
+
+export default Metronome;

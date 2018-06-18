34b52abc054fbda004eea09a0e31b441db8f7626
amsedivy <amsedivy@gmail.com>
2018-06-18 17:34:42 -0400

    reduced to simple functional return
    until further notice

--- src/client/view/components/instrument.js
+++ src/client/view/components/instrument.js
@@ -1,31 +1,9 @@
 import React from 'react';
-import Type from 'prop-types';
 import InstrumentTypes from '../../../model/enum/instrumentTypes';
 
-/*
-  class Instrument is the visual element representing a single playback stream
- */
-class Instrument extends React.Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
-    this.id = props.id;
-    this.performer = InstrumentTypes[Math.floor(Math.random() * InstrumentTypes.length)];
-  }
-
-  render() {
-    return <div><b>{`${this.performer}`}</b>{`, section no.${this.props.section + 1}`}</div>;
-  }
+function instrumental(props) {
+  const performer = InstrumentTypes[Math.floor(Math.random() * InstrumentTypes.length)];
+  return <div className="instrument"><b>{performer}</b>{` section: ${props.section + 1}`}</div>;
 }
 
-export default Instrument;
-
-Instrument.propTypes = {
-  section: Type.number,
-  id: Type.string,
-};
-
-Instrument.defaultProps = {
-  section: 0,
-  id: String(Math.floor(Math.random() * 100)),
-};
+export default instrumental;

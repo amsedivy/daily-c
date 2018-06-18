2cc066fa2a169ff1e7fbacd43e9ff819081c8b07
amsedivy <amsedivy@gmail.com>
2018-06-18 17:34:08 -0400

    import classes

--- src/client/view/browser.js
+++ src/client/view/browser.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './index';
+import '../styles/_core.scss';
 
 if (typeof window !== 'undefined') {
   ReactDom.render(<App />, document.getElementById('root'));

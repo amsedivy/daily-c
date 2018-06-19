1dbb6a5af8e0c72fc64078b0802a39c20745f48a
amsedivy <amsedivy@gmail.com>
2018-06-19 12:35:27 -0400

    instrument seleciton in the stream class

--- src/client/audio/playbackStream.js
+++ src/client/audio/playbackStream.js
@@ -1,10 +1,14 @@
+import InstrumentTypes from '../../model/enum/instrumentTypes';
+
 class PlaybackStream {
   constructor(id) {
     this.id = id;
-    this.beginAt = Math.floor(Math.random() * 5);
+    this.beginAt = Math.round(Math.random());
+    this.instrument = InstrumentTypes[Math.floor(Math.random() * InstrumentTypes.length)];
   }
 
   get startPoint() { return this.beginAt; }
+  get voice() { return this.instrument; }
 }
 
 export default PlaybackStream;

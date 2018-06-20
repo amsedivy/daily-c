66c0f7dd39d7ae832987d8d9d25ad71dede230ea
amsedivy <amsedivy@gmail.com>
2018-06-20 12:59:52 -0400

    acitvate audio metronome
    using window.audocontext and osciallator

--- src/client/audio/metronome.js
+++ src/client/audio/metronome.js
@@ -1,3 +1,4 @@
+/* global window */
 import EventDispatcher from '../../control/centralDispatch';
 import EventTypes from '../../model/enum/eventTypes';
 
@@ -10,12 +11,16 @@ class Metronome {
 
   start() {
     // begin the interval
+    this.setAudioContext();
+    this.createOscillator();
+
     this.counter = setInterval(() => {
       // dispatch the beat event
       EventDispatcher.dispatchEvent(EventTypes.BEAT);
 
       // keep track of the place in a 'bar' or 4/4 music
       this.barCounter += 1;
+      this.playBeat(this.barCounter === 1);
 
       // if we've reached the end of the bar, reset the counter and dispatch the bar event
       if (this.barCounter === 4) {
@@ -28,6 +33,41 @@ class Metronome {
   stop() {
     // stop the interval
     clearInterval(this.counter);
+  }
+
+  playBeat(firstBeat) {
+    if (!this.context) return;
+    const { context, osc, amp } = this;
+    const now = context.currentTime;
+    const pitch = (firstBeat) ? 280 : 200;
+
+    osc.frequency.setValueAtTime(pitch, now);
+    amp.gain.cancelScheduledValues(now);
+    amp.gain.setValueAtTime(amp.gain.value, now);
+    amp.gain.linearRampToValueAtTime(0.5, context.currentTime + 0.01);
+    amp.gain.linearRampToValueAtTime(0.0, context.currentTime + 0.11);
+  }
+
+  createOscillator() {
+    this.osc = this.context.createOscillator();
+    this.osc.frequency.value = 440;
+    this.amp = this.context.createGain();
+    this.amp.gain.value = 0;
+
+    this.osc.connect(this.amp);
+    this.amp.connect(this.context.destination);
+    this.osc.start(0);
+  }
+
+  setAudioContext() {
+    const Context = (
+      window.AudioContext ||
+      window.webkitAudioContext ||
+      window.mozAudioContext ||
+      window.oAudioContext);
+    if (Context) {
+      this.context = new Context();
+    }
   }
 }
 

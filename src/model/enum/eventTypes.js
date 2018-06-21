5e734ad802b3454721de5d2e9c551c25050c0f30
amsedivy <amsedivy@gmail.com>
2018-06-21 14:16:50 -0400

    new event type

--- src/model/enum/eventTypes.js
+++ src/model/enum/eventTypes.js
@@ -3,6 +3,7 @@ export default {
   INIT_COMPLETE: 'viewInitialized',
   BEAT: 'beat',
   BAR: 'barComplete',
+  ADD_REPEAT: 'addRepeat',
   ADVANCE: 'advande',
   SECTION_COMPLETE: 'sectionComplete',
   INSTRUMENT_COMPLETE: 'instrumentCompletedWork',
